6112da4dc2b9f30ae305ea900e2b0b963acd7c7f
Arli Lazaj <alazaj@ritech.co>
2025-03-05 18:57:28 +0100

    feat:add migrations

---
+++ Movie.Infrastructure/Migrations/20250305105438_AddTableForRent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movie.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableForRent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PriceStatus",
+                table: "Purchase",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RentPrice",
+                table: "Movie",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceStatus",
+                table: "Purchase");
+
+            migrationBuilder.DropColumn(
+                name: "RentPrice",
+                table: "Movie");
+        }
+    }
+}
